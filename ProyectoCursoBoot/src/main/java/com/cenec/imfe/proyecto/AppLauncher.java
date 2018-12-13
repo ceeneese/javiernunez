7bd548c3f59fe120ff49e17c19240f576ac1e98e
usuario <usuario@192.168.245.129>
2018-12-13 18:24:13 +0100

    Uso de Hibernate en lugar de JPA

--- ProyectoCursoBoot/src/main/java/com/cenec/imfe/proyecto/AppLauncher.java
+++ ProyectoCursoBoot/src/main/java/com/cenec/imfe/proyecto/AppLauncher.java
@@ -1,11 +1,19 @@
 package com.cenec.imfe.proyecto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { 
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class })
 public class AppLauncher extends SpringBootServletInitializer {
 	
 	@Override
